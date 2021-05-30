31a8c453b6a28647d8baee5ff938002d4397b8cc
supreeth1816 <supreet.venkata@gmail.com>
2021-05-30 18:34:12 +0530

    Modified layout

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -2,14 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
+
+
+
 class HomeScreen extends StatefulWidget {
+
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+
   @override
   Widget build(BuildContext context) {
+
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+
+
     return Scaffold(
       backgroundColor: Color(0xffffffff),
       body: SafeArea(
@@ -29,18 +42,58 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),),
               ),
 
-              SizedBox(height: 38,),
+              SizedBox(height: 28,),
 
               Container(
                 height: 186,
                 margin: EdgeInsets.only(left: 28, right: 28),
 
                 decoration: BoxDecoration(
-                  color: Colors.pink,
+                  color: Colors.white,
+                  border: Border.all(color: Color(0xffe2e2e2), width: 2),
                   borderRadius: BorderRadius.circular(15),
 
                 ),
               ),
+
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Container(
+                    width: w/2,
+                    height: 60,
+                    child: Row(
+                      children: [
+
+
+                        Text("Speak", style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xfff96e90),
+                        ),),
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    //width: w/2,
+                    child: Row(
+                      children: [
+
+                        Text("Save", style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff808080),
+                        ),),
+                      ],
+
+                    ),
+                  ),
+
+                ],
+              )
             ],
           ),
         ),
