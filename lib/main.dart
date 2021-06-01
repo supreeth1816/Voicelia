fe784beb22ed1c65cc5995dc49a9b1137053320b
supreeth1816 <supreet.venkata@gmail.com>
2021-06-02 04:15:13 +0530

    Modified Launch Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:voicelia/autismTest.dart';
 import 'package:voicelia/homeScreen.dart';
 
@@ -28,19 +29,54 @@ class _StartScreenState extends State<StartScreen> {
         child: Container(
           child: Column(
             children: [
-              SizedBox(height: 220,),
+              SizedBox(height: 120),
+              Text(" MGT1022 - LSM PROJECT ",
+                style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 0,),
+              Text(" Team #2 ",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(height: 40,),
 
               Image(image: AssetImage('assets/LaunchLogo.png'),
               height: 238,),
-              SizedBox(height: 240,),
+              SizedBox(height: 0,),
+              Text(" Autism Therapy Application ",
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 14
+              ),
+              ),
+
+
+
+              SizedBox(height: 220,),
 
               GestureDetector(
                 onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen())),
                 child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+
+                    color: Colors.white,
+                  ),
                   width: 340,
                   height: 48,
-                  color: Colors.white,
-                  child: Center(child: Text("Get Started")),
+
+                  child: Center(child: Text("Get Started", style: GoogleFonts.poppins(
+                    color:  Colors.black87,
+                    fontWeight: FontWeight.w600,
+                  ),)),
+
                 ),
               ),
 
